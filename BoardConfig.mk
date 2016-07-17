f7edfcd1d336bd9593f74d7ae6cc73a9b49cbbab
tetsuo55 <tetsuo55@users.noreply.github.com>
2016-07-17 21:40:18 +0200

    Remove TARGET_CPU_SMP
    No longer a valid option since commit https://android.googlesource.com/platform/bionic/+/bfbf7a4300ab5cd6334212f701e47ea449456048

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,6 @@ TARGET_BOARD_PLATFORM := tegra
 
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a9
