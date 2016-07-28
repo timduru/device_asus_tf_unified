d10498d55df7e54fff308498d08f8c71f759ff4f
tetsuo55 <tetsuo55@users.noreply.github.com>
2016-07-28 22:44:30 +0200

    Reformat using Astyle

--- wifimacwriter/wifimacwriter.c
+++ wifimacwriter/wifimacwriter.c
@@ -5,6 +5,7 @@
 * To Public License, Version 2, as published by Sam Hocevar. See
 * http://sam.zoy.org/wtfpl/COPYING for more details. */
 //
+
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mount.h>
@@ -19,43 +20,59 @@
 #define LOG_TAG "Wifimacwriter"
 #include <cutils/log.h>
 
-char* read_file(const char *fn)
+char* read_file(const char* fn)
 {
     struct stat st;
-    char *data = NULL;
+    char* data = NULL;
 
     int fd = open(fn, O_RDONLY);
-    if (fd < 0) return data;
 
-    if (fstat(fd, &st)) goto oops;
+    if (fd < 0) {
+        return data;
+    }
+
+    if (fstat(fd, &st)) {
+        goto oops;
+    }
 
     data = malloc(st.st_size + 1);
-    if (!data) goto oops;
 
-    if (read(fd, data, st.st_size) != st.st_size) goto oops;
+    if (!data) {
+        goto oops;
+    }
+
+    if (read(fd, data, st.st_size) != st.st_size) {
+        goto oops;
+    }
+
     close(fd);
     data[st.st_size + 1] = 0;
     return data;
 
 oops:
     close(fd);
-    if (data) free(data);
+
+    if (data) {
+        free(data);
+    }
+
     return NULL;
 }
 
-int copy_nvram(char *src) {
+int copy_nvram(char* src)
+{
 
     struct stat st;
-    char *mac = NULL;
-    char *nvram = NULL;
-    char *out = NULL;
+    char* mac = NULL;
+    char* nvram = NULL;
+    char* out = NULL;
     char dest[] = "/data/misc/wifi/nvram.txt";
     char mac_addr[] = "\nmacaddr=\0";
     char over_ride[] = "\nnvram_override=1\n\0";
     int fd = 0;
     int err = 0;
 
-    if (stat(dest, &st) == 0 ) {
+    if (stat(dest, &st) == 0) {
         SLOGI("%s exists", dest);
         goto permissions;
     }
@@ -68,12 +85,13 @@ int copy_nvram(char *src) {
         goto exit;
     }
 
-    if (stat("/data/wifimac", &st) != 0 ) {
+    if (stat("/data/wifimac", &st) != 0) {
         if (mkdir("/data/wifimac", S_IRWXU | S_IRWXG | S_IRWXO) < 0) {
             SLOGE("Failed to create mount point /data/wifimac %s", strerror(errno));
             err = 1;
             goto write;
         }
+
     } else {
         chown("/data/wifimac", 0, 0);
         chmod("/data/wifimac", S_IRWXU | S_IRWXG | S_IRWXO);
@@ -104,6 +122,7 @@ int copy_nvram(char *src) {
     sprintf(out, "%s%s%s%s", nvram, mac_addr, mac, over_ride);
 
 write:
+
     if (stat("/data/misc", &st) != 0) {
         SLOGW("Creating directory /data/misc");
         mkdir("/data/misc", S_IRWXU | S_IRWXG);
@@ -116,7 +135,7 @@ write:
         chown("/data/misc/wifi", 1010, 1010);
     }
 
-    fd = open(dest, O_CREAT|O_WRONLY|O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    fd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
     if (fd < 0) {
         SLOGE("Failed to open %s %s", dest, strerror(errno));
@@ -126,35 +145,49 @@ write:
 
     if (!out) {
         write(fd, nvram, strlen(nvram));
+
     } else {
         write(fd, out, strlen(out));
     }
+
     close(fd);
 
 permissions:
+
     if (chown(dest, 1000, 1010) < 0) {
         SLOGE("Failed to set owner/group %s %s", dest, strerror(errno));
         err = 1;
     }
+
     if (chmod(dest, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) < 0) {
         SLOGE("Failed to set permissions %s %s", dest, strerror(errno));
         err = 1;
     }
 
 exit:
-    if (mac) free(mac);
-    if (nvram) free(nvram);
-    if (out) free(out);
+
+    if (mac) {
+        free(mac);
+    }
+
+    if (nvram) {
+        free(nvram);
+    }
+
+    if (out) {
+        free(out);
+    }
+
     umount("/data/wifimac");
     rmdir("/data/wifimac");
     return err;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    FILE *fstream = NULL;
+    FILE* fstream = NULL;
     char project_id[16] = { 0 };
-    char *src = NULL;
+    char* src = NULL;
     int err = 0;
 
     fstream = fopen("/sys/devices/platform/cardhu_misc/cardhu_projectid", "r");
@@ -170,7 +203,7 @@ int main(int argc, char *argv[])
 
     SLOGI("Found project id %s", project_id);
 
-    switch(atoi(project_id)) {
+    switch (atoi(project_id)) {
 
         case 0x00:  // TF201
         case 0x02:  // TF300T
@@ -178,23 +211,23 @@ int main(int argc, char *argv[])
         case 0x05:  // TF300TL
             src = "/system/etc/nvram_nh615.txt";
             err = copy_nvram(src);
-            property_set("ro.epad.model_id",project_id);
-            property_set("wifi.module.type","1");
-            property_set("wlan.driver.p2p","0");
+            property_set("ro.epad.model_id", project_id);
+            property_set("wifi.module.type", "1");
+            property_set("wlan.driver.p2p", "0");
             break;
 
         case 0x04:  // TF700T
             src = "/system/etc/nvram_nh665.txt";
             err = copy_nvram(src);
-            property_set("ro.epad.model_id","04");
-            property_set("wifi.module.type","2");
+            property_set("ro.epad.model_id", "04");
+            property_set("wifi.module.type", "2");
             break;
 
         case 0x0b:  // ME301T
             src = "/system/etc/nvram_murata_4334.txt";
             err = copy_nvram(src);
-            property_set("ro.epad.model_id","0b");
-            property_set("wifi.module.type","5");
+            property_set("ro.epad.model_id", "0b");
+            property_set("wifi.module.type", "5");
             break;
 
         default:
@@ -204,10 +237,13 @@ int main(int argc, char *argv[])
     }
 
 exit:
+
     if (err == 0) {
         SLOGI("Completed without error");
+
     } else {
         SLOGE("Completed with error(s)");
     }
+
     return err;
 }
